6a1da6be2a22297e7e7c9ff72d375c8247417033
Octavio Bustos Solis <tavitomijo90@gmail.com>
2022-11-26 17:58:04 -0600

    Ultima Actualizacion

--- app/src/main/java/com/example/androidfingerprint/MainActivity.kt
+++ app/src/main/java/com/example/androidfingerprint/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.androidfingerprint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
