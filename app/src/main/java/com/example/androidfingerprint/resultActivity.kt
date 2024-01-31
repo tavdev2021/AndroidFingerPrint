b8605865558c8093670df0298c15d44e1d8b0eea
macbookpro <tavitomijo90@gmail.com>
2024-01-31 14:53:12 -0600

    Se agrego Custom Dialog para cerrar la app

--- app/src/main/java/com/example/androidfingerprint/resultActivity.kt
+++ app/src/main/java/com/example/androidfingerprint/resultActivity.kt
@@ -1,12 +1,53 @@
 package com.example.androidfingerprint
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
+import com.example.androidfingerprint.databinding.ActivityResultBinding
+import kotlin.system.exitProcess
 
 class resultActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btncerrar.setOnClickListener {
+        val message : String? = "Esta seguro que desea cerrar la aplicacion?"
+        showCustomDialogBox(message)
+        }
+    }
+
+    private fun showCustomDialogBox(message: String?) {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.layout_custom_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val tvMessage : TextView = dialog.findViewById(R.id.tvMessage)
+        val btnYes : Button = dialog.findViewById(R.id.btnYes)
+        val btnNo : Button = dialog.findViewById(R.id.btnNo)
+
+        tvMessage.text = message
+
+        btnYes.setOnClickListener {
+            exitProcess(0)
+        }
+
+        btnNo.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+
     }
 }
